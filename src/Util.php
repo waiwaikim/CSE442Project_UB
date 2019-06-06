7d034064e70c8048f50edc0c263933c5929ed2e2
Jacob M. Siegel <jacobsiegel35@gmail.com>
2019-06-05 20:07:15 -0400

    Tests now passing for email validation function #12

--- src/Util.php
+++ src/Util.php
@@ -2,7 +2,19 @@
 	declare(strict_types=1);
 	final class Util { 
 		public static function is_valid_email(string $email): bool {
-			return FALSE;
+			$ubit = strtok($email, '@');
+			$rest = strtok('');
+			
+			// first check if '@' exists in string
+			if (($at = strpos($email, "@")) == FALSE) return FALSE;
+			
+			// check that ubit is 8 characters
+			if (strlen($ubit) !== 8) return FALSE;
+
+			// ensure that email is from UB
+			if ($rest !== 'buffalo.edu') return FALSE;
+
+			return TRUE;
  		}
 	} 
 ?>
