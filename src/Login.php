e8483ac8a5bafb4b8bea052b9582ee10f73fa138
Waiwai <waiwai.j.kim@gmail.com>
2019-06-11 17:13:37 -0400

    #19 write and read to sql table

--- src/Login.php
+++ src/Login.php
@@ -1,4 +1,6 @@
-<?php include('Util.php'); ?>
+<?php include('ValidateEmail.php'); ?>
+<?php include('LoginSQL.php'); ?>
+
 
 <?php
   if (isset($_POST['submit'])) {
@@ -8,36 +10,26 @@
 
       echo 'A confirmation code has been sent to ' ;
       echo $email;
-      mail($email, "Course Evaluation Confirmation", "Welcome! Your confirmation code is N38DNJ9");
+        
+    
+      //$code = get_code();
+      //dummy code is hardcoded until hash function is fixed     
+      $code = "dummy123_until_fix";
+        
+      $conn = sqlConnect(); 
+      insertEmail($conn, $email, $code);
+        
+    
+      $conn = sqlConnect(); 
+      $code = getConfirmCode($conn, $email);
+    
+      mail($email, "Course Evaluation Confirmation", "Welcome! Your confirmation code is ".$code);
+      header('Location: https://www-student.cse.buffalo.edu/CSE442-542/2019-Summer/cse-442d/confirmation.html');
+        
+        
     } else {
       echo "Please enter a valid University at Buffalo email address.";
     }
-
-
-    $servername = "tethys.cse.buffalo.edu";
-    $username = 'waiwaiki';
-    $password = '50180101';
-    $database = 'cse442_542_2019_summer_teamd_db';
-
-    $conn = new mysqli($servername, $username, $password, $database) or die ("Connection failed: " . mysqli_connect_error());
-
-    echo "<br> Connected Succesfully";
-
-    $code = 'dummy_confirm_code'; 
-    // to be replaced by a real code
-    // import confirmation code generation code 
-
-    $sql = "INSERT INTO EmailTest (email, code) 
-            VALUES ('$email',  '$code')";
-
-    if($conn->query($sql) == TRUE){
-            echo "<br> New record created successfully";
-        }
-        else {
-            echo "<br> Error: " . $sql . "<br>" . $conn->error;
-        }
-
-    $conn -> close();
 
   }
 ?>
