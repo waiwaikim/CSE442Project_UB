088367d9fa2bccfbb694f007f4a5e12dc5082fe5
Luke McDaniel <lukemcda@buffalo.edu>
2019-06-11 11:39:02 -0400

    Update Login.php
    editing dummy variables to confirm get and pull requests are working on new machine.

--- src/Login.php
+++ src/Login.php
@@ -12,11 +12,11 @@
     } else {
       echo "Please enter a valid University at Buffalo email address.";
     }
-
+	//editing dummy variables
 
     $servername = "tethys.cse.buffalo.edu";
-    $username = 'waiwaiki';
-    $password = '50180101';
+    $username = 'lukemcda';
+    $password = '50188809';
     $database = 'cse442_542_2019_summer_teamd_db';
 
     $conn = new mysqli($servername, $username, $password, $database) or die ("Connection failed: " . mysqli_connect_error());
