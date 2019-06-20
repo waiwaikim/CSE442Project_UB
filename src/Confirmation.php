2072c8b4a3d73aca242677df3be5f3f8d302949d
Frank Tsai <dailyuse716@gmail.com>
2019-06-19 21:31:25 -0400

    Implemented evaluation form #38

--- src/Confirmation.php
+++ src/Confirmation.php
@@ -1,5 +1,7 @@
 <?php include_once("LoginSQL.php"); ?>
 <?php include_once("ConfirmationCode.php"); ?>
+<?php include_once("submissionSQL.php"); ?>
+<?php include_once("EvalForm.php"); ?>
 
 <?php
   $timeout = 15;
@@ -11,7 +13,7 @@
 
     if ($input == $code) {
       if (time() - ConfirmationCode::get_time($code) <= $timeout * 60) {
-        echo "Logged in successfully!";
+        echo get_form(getTeammates($email));
       } else {
         echo "Failed to log in: Your code is too old. Please request a new one.";
       }
--- src/EvalForm.php
+++ src/EvalForm.php
@@ -5,56 +5,46 @@ function get_options($name) {
   '<div id="role">
     <label style="margin-left: 10em";><h5>Role</h5></label><br>
     <label>Worst</label>
-      <form style="margin-left: 2.5em";>
-        <input type="radio" class="radio" name="role" id="role0" value="0points"> Rarely completes assigned work<br>
-        <input type="radio" class="radio" name="role" id="role1" value="1point"> Occasionally completes work<br>
-        <input type="radio" class="radio" name="role" id="role2" value="2point"> Mostly completes assigned work<br>
-        <input type="radio" class="radio" name="role" id="role3" value="3point"> Always completes assigned work
-      </form>
+    <input type="radio" class="radio" name="role'.$name.'" id="role0'.$name.'" value="0points"> Rarely completes assigned work<br>
+    <input type="radio" class="radio" name="role'.$name.'" id="role1'.$name.'" value="1point"> Occasionally completes work<br>
+    <input type="radio" class="radio" name="role'.$name.'" id="role2'.$name.'" value="2point"> Mostly completes assigned work<br>
+    <input type="radio" class="radio" name="role'.$name.'" id="role3'.$name.'" value="3point"> Always completes assigned work
     <label>Best</label>
   </div><br><br><br>
   <div id="Leadership">
     <label style="margin-left: 10em";><h5>Leadership</h5></label><br>
     <label>Worst</label>
-      <form style="margin-left: 2.5em";>
-        <input type="radio" class="radio" name="leadership" id="leadership0" value="0points"> Rarely takes leadership role; Does not collaborate<br>
-        <input type="radio" class="radio" name="leadership" id="leadership1" value="1point"> Occasionally shows leadership; Mostly collaborates <br>
-        <input type="radio" class="radio" name="leadership" id="leadership2" value="2point"> Shows an ability to lead when necessary; Willing to assist teammates<br>
-        <input type="radio" class="radio" name="leadership" id="leadership3" value="3point"> Takes leadership role; Is a good collaborator
-      </form>
+        <input type="radio" class="radio" name="leadership'.$name.'" id="leadership0'.$name.'" value="0points"> Rarely takes leadership role; Does not collaborate<br>
+        <input type="radio" class="radio" name="leadership'.$name.'" id="leadership1'.$name.'" value="1point"> Occasionally shows leadership; Mostly collaborates <br>
+        <input type="radio" class="radio" name="leadership'.$name.'" id="leadership2'.$name.'" value="2point"> Shows an ability to lead when necessary; Willing to assist teammates<br>
+        <input type="radio" class="radio" name="leadership'.$name.'" id="leadership3'.$name.'" value="3point"> Takes leadership role; Is a good collaborator
     <label>Best</label>
   </div><br><br><br>
   <div id="Participation">
     <label style="margin-left: 10em";><h5>Participation</h5></label><br>
     <label>Worst</label>
-      <form style="margin-left: 2.5em";>
-        <input type="radio" class="radio" name="participation" id="participation0" value="0points"> Rarely participates in meeting and does not share ideas<br>
-        <input type="radio" class="radio" name="participation"  id="participation1" value="1point"> Offers unclear or unhelpful ideas in meetings<br>
-        <input type="radio" class="radio" name="participation"  id="participation2" value="2point"> Attends and particpates in most meetings; Offer useful ideas in meetings<br>
-        <input type="radio" class="radio" name="participation"  id="participation3" value="3point"> Attends and participates in all meetings; Clearly expresses well developed ideas
-      </form>
+        <input type="radio" class="radio" name="participation'.$name.'" id="participation0'.$name.'" value="0points"> Rarely participates in meeting and does not share ideas<br>
+        <input type="radio" class="radio" name="participation'.$name.'"  id="participation1'.$name.'" value="1point"> Offers unclear or unhelpful ideas in meetings<br>
+        <input type="radio" class="radio" name="participation'.$name.'"  id="participation2'.$name.'" value="2point"> Attends and particpates in most meetings; Offer useful ideas in meetings<br>
+        <input type="radio" class="radio" name="participation'.$name.'"  id="participation3'.$name.'" value="3point"> Attends and participates in all meetings; Clearly expresses well developed ideas
     <label>Best</label>
   </div><br><br><br>
   <div id="Professionalism">
   <label style="margin-left: 10em";><h5>Professionalism</h5></label><br>
     <label>Worst</label>
-      <form style="margin-left: 2.5em";>
-        <input type="radio" class="radio" name="prof" id="professionalism0" value="0points"> Often discourteous and/or openly critical of teammates <br>
-        <input type="radio" class="radio" name="prof" id="professionalism1" value="1point"> Usually appreciates teammates\' perspective, but often unwilling to consider them<br>
-        <input type="radio" class="radio" name="prof" id="professionalism2" value="2point"> Values teammates\' perspectives and often willing to consider them<br>
-        <input type="radio" class="radio" name="prof" id="professionalism3" value="3point"> Always courteous to teammates
-      </form>
+        <input type="radio" class="radio" name="prof'.$name.'" id="professionalism0'.$name.'" value="0points"> Often discourteous and/or openly critical of teammates <br>
+        <input type="radio" class="radio" name="prof'.$name.'" id="professionalism1'.$name.'" value="1point"> Usually appreciates teammates\' perspective, but often unwilling to consider them<br>
+        <input type="radio" class="radio" name="prof'.$name.'" id="professionalism2'.$name.'" value="2point"> Values teammates\' perspectives and often willing to consider them<br>
+        <input type="radio" class="radio" name="prof'.$name.'" id="professionalism3'.$name.'" value="3point"> Always courteous to teammates
     <label>Best</label>
   </div><br><br><br>
   <div id="Quality">
     <label style="margin-left: 10em";><h5>Quality</h5></label><br>
     <label>Worst</label>
-      <form style="margin-left: 2.5em";>
-        <input type="radio" class="radio" name="quality" id="quality0" value="0points"> Rarely commits to shared documents<br>
-        <input type="radio" class="radio" name="quality" id="quality1" value="1point"> Other sometimes needed to revise, debug, or fix their work<br>
-        <input type="radio" class="radio" name="quality" id="quality2" value="2point"> Often commits to shared documents<br>
-        <input type="radio" class="radio" name="quality" id="quality3" value="3point"> Frequently commits to shared documents
-      </form>
+        <input type="radio" class="radio" name="quality'.$name.'" id="quality0'.$name.'" value="0points"> Rarely commits to shared documents<br>
+        <input type="radio" class="radio" name="quality'.$name.'" id="quality1'.$name.'" value="1point"> Other sometimes needed to revise, debug, or fix their work<br>
+        <input type="radio" class="radio" name="quality'.$name.'" id="quality2'.$name.'" value="2point"> Often commits to shared documents<br>
+        <input type="radio" class="radio" name="quality'.$name.'" id="quality3'.$name.'" value="3point"> Frequently commits to shared documents
     <label>Best</label>
   </div><br><br>';
 }
@@ -70,7 +60,5 @@ function get_form($names) {
 
   return $top.$form.$bottom;
 }
-
-echo get_form(array("Frank", "John", "Someone", "Else"));
 
 ?>
